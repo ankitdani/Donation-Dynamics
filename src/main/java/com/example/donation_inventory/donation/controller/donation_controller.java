b4dffb9254ee9985b5224efc275064ae7b8821d2
Ankit Dani <ankitdani1997@gmail.com>
2024-01-14 11:56:48 -0800

    Added save logic for donor details  using donor repository

--- src/main/java/com/example/donation_inventory/donation/controller/donation_controller.java
+++ src/main/java/com/example/donation_inventory/donation/controller/donation_controller.java
@@ -6,6 +6,7 @@
     import org.springframework.web.bind.annotation.GetMapping;
     import org.springframework.web.bind.annotation.PostMapping;
     import com.example.donation_inventory.donation.Repository.donor_repository;
+    import com.example.donation_inventory.donation.entity.donor;
 
     @Controller
     public class donation_controller {
@@ -17,6 +18,17 @@
         }
         @PostMapping(path = "/donation_inventory_form")
         public String submit_donation_inventory_form(donation_inventory_form donation_inventory_form){
+
+            donor donor = null;
+
+            if (donation_inventory_form != null){
+                donor = new donor(donation_inventory_form.getDonor_name(),
+                        donation_inventory_form.getDonation_type(),
+                        donation_inventory_form.getDonation_amount(),
+                        donation_inventory_form.getDonation_date());
+            }
+            donor_repository.save(donor);
+
             return "donation_inventory_form_summary";
         }
 
--- src/main/java/com/example/donation_inventory/donation/donation_inventory_form.java
+++ src/main/java/com/example/donation_inventory/donation/donation_inventory_form.java
@@ -15,4 +15,36 @@ public class donation_inventory_form {
         this.donation_amount = donation_amount;
         this.donation_date = donation_date;
     }
+
+    public String getDonor_name() {
+        return donor_name;
+    }
+
+    public void setDonor_name(String donor_name) {
+        this.donor_name = donor_name;
+    }
+
+    public String getDonation_type() {
+        return donation_type;
+    }
+
+    public void setDonation_type(String donation_type) {
+        this.donation_type = donation_type;
+    }
+
+    public long getDonation_amount() {
+        return donation_amount;
+    }
+
+    public void setDonation_amount(long donation_amount) {
+        this.donation_amount = donation_amount;
+    }
+
+    public String getDonation_date() {
+        return donation_date;
+    }
+
+    public void setDonation_date(String donation_date) {
+        this.donation_date = donation_date;
+    }
 }
